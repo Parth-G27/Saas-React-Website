d9069480bac6c24f1eff18c89d5929e9f9cecdc5
PARTH <parthbidari13@gmail.com>
2023-03-08 17:14:45 +0530

    Text animation

--- src/pages/HomeThree/Textanimation/Textanimation.jsx
+++ src/pages/HomeThree/Textanimation/Textanimation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TextAnimation from 'react-text-animations'
 
 
 const Textanimation = () => {
@@ -8,18 +9,10 @@ const Textanimation = () => {
             <div className="container">
                <div className="row">
                   <div className="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-                     <div className="section__title section__title-3 text-center mt-100 mb-10 wow fadeInUp" data-wow-delay=".2s">
+                     <div className="section__title section__title-an text-center mt-100 mb-10 wow fadeInUp" data-wow-delay=".2s">
                         <div className='pb-40'>
-                            <h2>The Future of Data Science is No Code</h2>
-                        </div>
-                        <h4 className='news'>Build Data Driven Workforce using No Code Data Intelligence Platform.</h4>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </section>
-         {/* <TextAnimation.Scale
-                      className="span block dark:text-indigo-400 text-blue-600 mt-3 mb-3 "
+                            <h2>The Future of <TextAnimation.Scale
+                      className="mt-3 mb-3 "
                       animation={{
                         duration: 1500,
                         delay: 4000,
@@ -29,7 +22,16 @@ const Textanimation = () => {
                       text={['Analytics', 'Visualization', 'Science']}
                     >
                       Data Science
-                    </TextAnimation.Scale> */}
+                    </TextAnimation.Scale>
+                                 is No Code</h2>
+                        </div>
+                        <h4 className='news'>Build Data Driven Workforce using No Code Data Intelligence Platform.</h4>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         
           
      </>
     );
